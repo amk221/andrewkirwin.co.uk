276d166d29ae0662b08b42d25df7fc16eff2b51f
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-08-04 17:07:27 +0100

    remove references to twitter

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ class Intro {
   }
 
   sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
 
